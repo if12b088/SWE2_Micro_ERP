1ce722ace0823e7667d17dde87a84b92fe3b656b
Matthias Kreuzriegler <matthias.kreuzriegler@gmx.at>
2014-03-29 16:09:53 +0100

    libs added

--- SWE2_Micro_ERP_GUI/src/at/technikum/wien/winterhalder/kreuzriegler/swe2/gui/proxy/impl/ContactWebService.java
+++ SWE2_Micro_ERP_GUI/src/at/technikum/wien/winterhalder/kreuzriegler/swe2/gui/proxy/impl/ContactWebService.java
@@ -3,8 +3,6 @@ package at.technikum.wien.winterhalder.kreuzriegler.swe2.gui.proxy.impl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +16,6 @@ import org.apache.http.message.BasicNameValuePair;
 
 import at.technikum.wien.winterhalder.kreuzriegler.swe2.Uris;
 import at.technikum.wien.winterhalder.kreuzriegler.swe2.dto.ContactDto;
-import at.technikum.wien.winterhalder.kreuzriegler.swe2.gui.Constants;
 import at.technikum.wien.winterhalder.kreuzriegler.swe2.gui.proxy.IContactProxy;
 import at.technikum.wien.winterhalder.kreuzriegler.swe2.request.GetContactsBySearchstringRequest;
 import at.technikum.wien.winterhalder.kreuzriegler.swe2.response.GetContactsBySearchstringResponse;
