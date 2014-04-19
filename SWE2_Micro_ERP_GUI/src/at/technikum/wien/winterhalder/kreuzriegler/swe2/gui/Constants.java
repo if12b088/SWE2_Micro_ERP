545073e399fbfa708a554abc0c6f7f88654e42f4
Matthias Kreuzriegler <matthias.kreuzriegler@gmx.at>
2014-04-19 15:31:20 +0200

    merge

--- SWE2_Micro_ERP_GUI/src/at/technikum/wien/winterhalder/kreuzriegler/swe2/gui/Constants.java
+++ SWE2_Micro_ERP_GUI/src/at/technikum/wien/winterhalder/kreuzriegler/swe2/gui/Constants.java
@@ -1,8 +1,8 @@
 package at.technikum.wien.winterhalder.kreuzriegler.swe2.gui;
 
 public class Constants {
-	public static final int PORT= 8080;
-	public static final String HOST = "84.113.228.81";
+	public static final int PORT= 8088;
+	public static final String HOST = "localhost";
 	public static final String PATH = "/micro/";
-	public static final String SCHEME = "Http";
+	public static final String SCHEME = "http";
 }
--- SWE2_Micro_ERP_GUI/src/at/technikum/wien/winterhalder/kreuzriegler/swe2/gui/proxy/impl/ContactWebService.java
+++ SWE2_Micro_ERP_GUI/src/at/technikum/wien/winterhalder/kreuzriegler/swe2/gui/proxy/impl/ContactWebService.java
@@ -13,7 +13,6 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 
